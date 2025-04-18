a4be975d0ce0191e94eedff29e9c8133f297c1c9
JasminHT <JasminHT@users.noreply.github.com>
2025-04-18 17:44:09 -0400

    Add tests to World.js

--- test/World.test.js
+++ test/World.test.js
@@ -7,14 +7,32 @@ import Hex from '../src/u/Hex.js'
 import {Point, HexLayout, HexMap} from '../src/u/Hex.js'
 
 //create a world
-let earth_radius = 20;
-let earth_location = new Point(0, 0);
-var earth = new World( earth_radius, 'dust', earth_location );
+let world_radius = 20;
+let world_location = new Point(0, 0);
+var world = new World( world_radius, 'dust', world_location );
+let random_hex = world.getRandomHex();
 
-test('Check that hexes are there', () => {
-  expect(earth.containsHex(0,0)).toBe(true)
+test('World object created', () => {
+  expect(world.radius).toBe(20)
 })
 
+test('Origin hex exists', () => {
+  let hex = new Hex(0,0);
+  expect(world.containsHex(hex)).toBe(true)
+})
+
+test('Correct number of hexes', () => {
+  expect(world.tileCount()).toBe(955) //for a hexagon with 20 radius
+})
+
+test('Tiles are created', () => {
+  expect(world.getTile(random_hex).hasOwnProperty('elevation')).toBe(true)
+})
+
+test('Units can be created', () => {
+  world.createUnit(random_hex, 'city')
+  expect(world.getUnit(random_hex).name).toBe("City")
+})
 /*
 test('description', () => {
   expect().toBe()
